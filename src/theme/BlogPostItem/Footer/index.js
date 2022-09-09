be4b2e6b6275d36dddef9115804fbcc76eeb8d2c
Mikael Finstad <finstaden@gmail.com>
2022-09-09 22:18:56 +0200

    implement comments

---
+++ src/theme/BlogPostItem/Footer/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Footer from '@theme-original/BlogPostItem/Footer';
+import Giscus from '@giscus/react';
+
+import {useBlogPost} from '@docusaurus/theme-common/internal';
+
+export default function FooterWrapper(props) {
+  const { isBlogPostPage } = useBlogPost();
+
+  return (
+    <>
+      <Footer {...props} />
+      
+      {isBlogPostPage && (
+        <Giscus
+          repo="mifi/mifi.no"
+          repoId="R_kgDOH61uBw"
+          category="Announcements"
+          categoryId="DIC_kwDOH61uB84CRWKh"
+          mapping="pathname"
+          strict="1"
+          reactionsEnabled="1"
+          emitMetadata="0"
+          inputPosition="bottom"
+          theme="preferred_color_scheme"
+          lang="en"
+          loading="lazy"
+        />
+      )}
+    </>
+  );
+}
