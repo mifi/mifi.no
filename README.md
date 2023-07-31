bcd0650e82ac8ad26a83088f2bbbdb7085e9f026
Mikael Finstad <finstaden@gmail.com>
2023-07-31 23:26:02 +0200

    add script

--- README.md
+++ README.md
@@ -18,6 +18,12 @@ $ yarn start
 
 This command starts a local development server and opens up a browser window. Most changes are reflected live without having to restart the server.
 
+### Create blog post
+
+```
+yarn make-blogpost 'My blogpost title'
+```
+
 ### Build
 
 ```
---
+++ make-blogpost.mjs
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+import assert from 'assert';
+import { writeFile } from 'fs/promises';
+
+const author = 'mifi';
+
+const title = process.argv[2];
+assert(title, 'Title needed');
+
+// https://stackoverflow.com/a/1054862/6519037
+const slug = title.toLowerCase().replace(/ /g, '-').replace(/[-]+/g, '-').replace(/[^\w-]+/g, '');
+
+const esc = (str) => `'${str.replace(/'/g, "''")}'`;
+
+const md = `\
+---
+slug: ${slug}
+title: ${esc(title)}
+authors: ${esc(author)}
+tags: []
+---
+
+EXCERPT
+
+<!--truncate-->
+
+CONTENT
+
+`;
+
+const date = new Date().toISOString().split('T')[0];
+const filename = `${date}-${slug}.md`;
+
+await writeFile(new URL(`blog/${filename}`, import.meta.url), md);
