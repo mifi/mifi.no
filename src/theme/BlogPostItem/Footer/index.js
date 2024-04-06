39e803c3bb4dbbcdb9bec3417dc7b227c9ef8257
Mikael Finstad <finstaden@gmail.com>
2024-04-06 22:59:20 +0200

    fix

--- src/theme/BlogPostItem/Footer/index.js
+++ src/theme/BlogPostItem/Footer/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Footer from '@theme-original/BlogPostItem/Footer';
 import Giscus from '@giscus/react';
 
-import {useBlogPost} from '@docusaurus/theme-common/lib/internal.js';
+import {useBlogPost} from '@docusaurus/theme-common/internal';
 
 export default function FooterWrapper(props) {
   const { isBlogPostPage } = useBlogPost();
