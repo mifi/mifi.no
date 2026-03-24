a2bbdbf30019d3eb296ef051f5d48487a53b7c4c
Mikael Finstad <finstaden@gmail.com>
2026-03-24 14:55:14 +0800

    fix

--- src/pages/thanks/stripe.tsx
+++ src/pages/thanks/stripe.tsx
@@ -15,24 +15,24 @@ const wrapperStyle: CSSProperties = {
   padding: '40px 40px 0 40px',
 };
 
+export const chinaPayUrl = testMode ? 'https://donate.stripe.com/test_28o7uu1wOayg664dR0' : 'https://donate.stripe.com/5kAdS60q54svbQsfZ9';
+
 export default function Stripe() {
   return (
     <Layout title="Stripe donation">
       <div style={wrapperStyle}>
         <h1 style={{ fontWeight: 300, fontSize: '3em' }}>Stripe donation</h1>
 
-        <p style={{ maxWidth: 400 }}>If you would like to support my free / open source work via stripe, please select an option below. <b>Thank you.</b></p>
+        <p style={{ textAlign: 'center' }}>If you would like to support my free / open source work via stripe, please select an option below.</p>
 
-        <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-          <div style={{ margin: '20px 40px' }}>
-            <h2 style={{ fontWeight: 300 }}>Monthly support</h2>
-            <Link to={testMode ? 'https://donate.stripe.com/test_fZufZifiu7Kt5hN3Uico00c' : 'https://donate.stripe.com/7sY6oI8U69SB6lR8ayco00d'}>monthly support</Link>
-          </div>
+        <p style={{ textAlign: 'center' }}><b>Thank you! 🙏</b></p>
 
-          <div style={{ margin: '20px 40px' }}>
-            <Link to={testMode ? 'https://donate.stripe.com/test_00waEYc6i3udaC7duSco00b' : 'https://donate.stripe.com/fZufZifiu7Kt5hN3Uico00c'}>Donation</Link>
-            <Link to={testMode ? 'https://donate.stripe.com/test_28o7uu1wOayg664dR0' : 'https://donate.stripe.com/5kAdS60q54svbQsfZ9'}>WeChat Pay / 微信支付</Link>
-          </div>
+        <div style={{ display: 'flex', flexWrap: 'wrap', flexDirection: 'column', alignItems: 'center', gap: '.4em', fontSize: '1.3em' }}>
+          <Link to={testMode ? 'https://donate.stripe.com/test_00waEYc6i3udaC7duSco00b' : 'https://donate.stripe.com/fZufZifiu7Kt5hN3Uico00c'}>Donation</Link>
+
+          <Link to={testMode ? 'https://donate.stripe.com/test_fZufZifiu7Kt5hN3Uico00c' : 'https://donate.stripe.com/7sY6oI8U69SB6lR8ayco00d'}>Monthly support</Link>
+
+          <Link to={chinaPayUrl}>WeChat Pay / 微信支付</Link>
         </div>
 
         <Link style={{ margin: '30px 0' }} to="/thanks">Back</Link>
